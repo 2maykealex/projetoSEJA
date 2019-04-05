043393a508bfbd5ba9249118b85c09aa1bb37b33
Mayke Alex <mayke.suporte@gmail.com>
2019-04-04 21:16:04 -0400

    Criado grupo de rota para NameSpace Site

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+Route::group(['namespace' => 'Site', 'prefix' => '/'], function () {
+    // Route::get('home', 'SiteController@index')->name("site.home");
+});
+
 Route::get('/', function () {
     return view('index');
 });
