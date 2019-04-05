c451312f03dfd9ed9d5994b4d848dfb588696ae5
Mayke Alex <mayke.suporte@gmail.com>
2019-04-05 09:47:55 -0400

    Criado rotas para Área de não assinantes (inicial)

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 Route::group(['namespace' => 'Site', 'prefix' => '/'], function () {
-    // Route::get('home', 'SiteController@index')->name("site.home");
+    Route::get('', 'SiteController@index')->name("site.home");
+    Route::get('contatos', 'SiteController@contact')->name("site.contatos");
+    Route::get('quem-somos', 'SiteController@whoWeare')->name("site.quemsomos");
+    Route::get('onde-estamos', 'SiteController@whereWeAre')->name("site.localizacao");
+    Route::get('login', 'SiteController@doLogin')->name("site.login");
+    Route::get('inscreva-se', 'SiteController@subscribe')->name("site.subscribe");
 });
 
-Route::get('/', function () {
-    return view('index');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
