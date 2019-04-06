a5563a9c67518e7a75b389638b81f3329bc81609
Mayke Alex <mayke.suporte@gmail.com>
2019-04-06 08:50:28 -0400

    Create Migrations: States, Cities, People

---
+++ database/migrations/2019_04_06_123625_people.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class People extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users') ;
+
+            $table->string('name', 100);
+            $table->string('cpf', 11);
+            $table->string('telephone', 11)->unique();
+            $table->string('zipcode', 8);
+            $table->string('address', 100);
+            $table->integer('city_id')->unsigned();
+            $table->foreign('city_id')->references('id')->on('cities');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
