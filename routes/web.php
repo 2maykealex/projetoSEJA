f143a326ec53d33aab962f0bb80c3f47e4534d2b
Mayke Alex <mayke.suporte@gmail.com>
2019-04-15 21:42:11 -0400

    alterado rotas

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
-Route::group(['namespace' => 'Site', 'prefix' => '/'], function () {
+Route::group(['namespace' => 'Site', 'prefix' => '/site'], function () {
     Route::get('', 'SiteController@index')->name("site.home");
     Route::get('contatos', 'SiteController@contact')->name("site.contatos");
     Route::get('quem-somos', 'SiteController@whoWeare')->name("site.quemsomos");
     Route::get('onde-estamos', 'SiteController@whereWeAre')->name("site.localizacao");
-    Route::get('inscreva-se', 'SiteController@subscribe')->name("site.subscribe");
-    Route::post('subscribe/store', 'SiteController@subscribeStore')->name("site.subscribe.store");
-
-    Route::get('login', 'SiteController@doLogin')->name("site.login");
-    Route::post('login-validate', 'SiteController@validateLogin')->name("site.login.validate");
+    
 });
 
-Route::group(['namespace' => 'Adm', 'prefix' => 'adm'], function () {
+Route::group(['middleware' => ['auth', 'authRoute'], 'namespace' => 'Adm', 'prefix' => 'adm'], function () {
     Route::get('', 'AdmController@index')->name("adm.home");
     Route::get('new', 'AdmController@create')->name("adm.new");
     Route::post('new-save', 'AdmController@store')->name("adm.store");
@@ -21,6 +17,9 @@ Route::group(['namespace' => 'Adm', 'prefix' => 'adm'], function () {
     Route::post('update/{id}', 'AdmController@update')->name("adm.update");
 });
 
-// Route::get('/', function () {
-//     return view('index');
-// });
+Route::post('login-validate', 'Site\SiteController@validateLogin')->name("site.login.validate");
+Route::post('subscribe/store', 'Site\SiteController@subscribeStore')->name("site.subscribe.store");
+Route::get('inscreva-se', 'Site\SiteController@subscribe')->name("subscribe")->middleware('authRoute');
+Route::get('login', 'Site\SiteController@doLogin')->name("site.login");
+Route::get('logout', 'Auth\LoginController@logout');
+Route::get('/', 'Site\SiteController@index');
