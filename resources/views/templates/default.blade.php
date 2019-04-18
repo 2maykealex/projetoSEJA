b3c0373529d02a30d79e301551702887a01cfe3e
MACL <mateusalisson15@gmail.com>
2019-04-18 15:55:31 -0300

    alteração teste

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -7,8 +7,6 @@
     <title>Document @yield('title')</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    <script src="<?php echo asset('js/cities_valid.js')?>" type="text/javascript" charset="utf-8" async defer></script>
     
     <style>
 
