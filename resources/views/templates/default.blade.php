fa2d904ceb251c573f4b900cc36f8d4e96ef915c
Mayke Alex <mayke.suporte@gmail.com>
2019-04-18 00:41:14 -0400

    Ajustes default1

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -4,16 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document @yield('title')</title>
+    <title>@yield('title')</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <script src="<?php echo asset('js/cities_valid.js')?>" type="text/javascript" charset="utf-8" async defer></script>
-    
     <style>
-
-        @yield('styles')
-
         #inicio {
             background: url("{{url('storage/img/astro.jpg')}}") no-repeat center center fixed;
             display: table;
@@ -31,8 +26,7 @@
             width : 100%;
             background-color: rgba(0,0,0,.3) !important;
         }
-            
-    </style>    
+    </style>
 
 </head>
 <body>
