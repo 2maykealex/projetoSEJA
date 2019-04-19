ab09b9c9ed513054dbb536805fad82431594e998
MACL <mateusalisson15@gmail.com>
2019-04-19 19:25:46 -0300

    Criação do pasta adm e arquivos para pag do adm

---
+++ resources/views/adm/pagina_inicial_adm.blade.php
@@ -0,0 +1,9 @@
+@extends('templates.default_adm')
+
+@section('title')
+
+@endsection
+
+@section('content')
+	<h1>Deu certo</h1>
+@endsection
--- resources/views/site/subscribe.blade.php
+++ resources/views/site/subscribe.blade.php
@@ -1,7 +1,7 @@
 @extends('templates.default_2')
 
 @section('title')
-    
+
 @endsection
 
 @section('content')
@@ -15,38 +15,38 @@
 					<form action="{{route('subscriber.store')}}" method="POST" class="form form-" onsubmit="EnviarFormulario()">
 						@csrf
 						<div class="form-group row">
-						    <label class="col-md-3 col-form-label">Nome</label>
-						    <div class="col-md-9">
+						    <label class="col-md-2 col-form-label">Nome</label>
+						    <div class="col-md-10">
 						    	<input class="form-control" id="name" name="name">
 						    </div>
 						</div>
 						<div class="form-group row">
-						    <label class="col-md-3 col-form-label">CPF</label>
-						    <div class="col-md-9">
+						    <label class="col-md-2 col-form-label">CPF</label>
+						    <div class="col-md-10">
 						    	<input class="form-control" id="cpf" name="cpf" onInput="checkMaskDocument(this)">
 						    </div>
 						</div>
 						<div class="form-group row">
-						    <label class="col-md-3 col-form-label">Tel/Cel</label>
-						    <div class="col-md-9">
+						    <label class="col-md-2 col-form-label">Tel/Cel</label>
+						    <div class="col-md-10">
 						    	<input class="form-control" id="telephone" name="telephone" onInput="checkMaskTelephone(this)">
 						    </div>
 						</div>
 				    	<div class="form-group row">
-				    		<label for="inputCEP" class="col-md-3 col-form-label">CEP</label>
-				    		<div class="col-md-9">
+				    		<label for="inputCEP" class="col-md-2 col-form-label">CEP</label>
+				    		<div class="col-md-10">
 				      			<input type="text" class="form-control" id="zipcode" name="zipcode" onInput="checkMaskZipCode(this)">
 				      		</div>
 				    	</div>  	
 				    	<div class="form-group row">
-				    		<label for="inputCEP" class="col-md-3 col-form-label">Endereço</label>
-				    		<div class="col-md-9">
+				    		<label for="inputCEP" class="col-md-2 col-form-label">Endereço</label>
+				    		<div class="col-md-10">
 				      			<input type="text" class="form-control" id="address" name="address">
 				      		</div>
 				    	</div>
 				    	<div class="form-group row">
-				      		<label for="inputEstado" class="col-md-3 col-form-label">Estado</label>
-				      		<div class="col-md-9">
+				      		<label for="inputEstado" class="col-md-2 col-form-label">Estado</label>
+				      		<div class="col-md-10">
 					      		<select class="form-control" id="state" required onchange="listCities(this.value);" collapse>
 					        		<option value="-1">Escolher</option>
 					        		@foreach($states as $state)
@@ -56,8 +56,8 @@
 					      	</div>
 				    	</div>
 				    	<div class="form-group row">
-				      		<label for="inputCity" class="col-md-3 col-form-label">Cidade</label>
-				      		<div class="col-md-9">
+				      		<label for="inputCity" class="col-md-2 col-form-label">Cidade</label>
+				      		<div class="col-md-10">
 					      		<select id="city" name="city" class="form-control">
 					        		<option selected>Escolher</option>
 					        		<option>...</option>
@@ -65,20 +65,20 @@
 					      	</div>
 					  	</div>
 					  	<div class="form-group row">
-				    		<label  for="email" class="col-md-3 col-form-label">E-mail</label>
-				    		<div class="col-md-9">
+				    		<label  for="email" class="col-md-2 col-form-label">E-mail</label>
+				    		<div class="col-md-10">
 				      			<input type="email" class="form-control" id="email" name="email">
 				      		</div>
 				    	</div>  	
 				    	<div class="form-group row">
-				    		<label for="password" class="col-md-3 col-form-label">Senha</label>
-				    		<div class="col-md-9">
+				    		<label for="password" class="col-md-2 col-form-label">Senha</label>
+				    		<div class="col-md-10">
 				      			<input type="password" class="form-control" id="password" name="password">
 				      		</div>
 				    	</div>
 				    	<div class="form-group row">
-				    		<label for="confirmPassword" class="col-md-3 col-form-label">Confirmar</label>
-				    		<div class="col-md-9">
+				    		<label for="confirmPassword" class="col-md-2 col-form-label">Confirmar</label>
+				    		<div class="col-md-10">
 				      			<input type="password" class="form-control" id="confirmPassword">
 				      		</div>
 				    	</div>
