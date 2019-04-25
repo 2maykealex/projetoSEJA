1f188710ff569f71f222b821eefed75fd5b32509
Mayke Alex <mayke.suporte@gmail.com>
2019-04-25 18:01:16 -0400

    Criado Rotas/ Views/ Controller/ Model/ Seeder ->  CongressType

---
+++ resources/views/adm/congressType/index.blade.php
@@ -0,0 +1,21 @@
+@extends('templates.default_adm')
+
+@section('title')
+
+@endsection
+
+@section('content')
+	<div class="d-flex justify-content-center flex-lg-center">
+		<div class="col-md-8">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			  	<h2>TIPOS DE CONGRESSOS</h2>
+			  	<div class="ml-auto">
+			  		<button type="button" class="btn btn-primary btn-sm">Novo tipo</button>
+			  	</div>
+			</nav>
+	      	<div class="table-responsive">
+	        	
+	      	</div>
+	    </div>
+	</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -41,13 +41,13 @@ Route::group(['middleware' => ['auth', 'authRoute'], 'namespace' => 'Adm', 'pref
     });
     
     Route::group(['prefix' => 'congresses-types'], function () {
-        Route::get('list', 'CongressesTypeController@index')->name("adm.congresses.types.list");
-        Route::get('new', 'CongressesTypeController@create')->name("adm.congresses.types.new");
-        Route::get('show/{id}', 'CongressesTypeController@show')->name("adm.congresses.types.show");
-        Route::get('edit/{id}', 'CongressesTypeController@edit')->name("adm.congresses.types.edit");
-        Route::get('delete/{id}', 'CongressesTypeController@destroy')->name("adm.congresses.types.delete");
-        Route::post('update/{id}', 'CongressesTypeController@update')->name("adm.congresses.types.update");
-        Route::post('store/{id}', 'CongressesTypeController@store')->name("adm.congresses.types.store");
+        Route::get('list', 'CongressTypeController@index')->name("adm.congresses.types.list");
+        Route::get('new', 'CongressTypeController@create')->name("adm.congresses.types.new");
+        Route::get('show/{id}', 'CongressTypeController@show')->name("adm.congresses.types.show");
+        Route::get('edit/{id}', 'CongressTypeController@edit')->name("adm.congresses.types.edit");
+        Route::get('delete/{id}', 'CongressTypeController@destroy')->name("adm.congresses.types.delete");
+        Route::post('update/{id}', 'CongressTypeController@update')->name("adm.congresses.types.update");
+        Route::post('store/{id}', 'CongressTypeController@store')->name("adm.congresses.types.store");
     });
 
     Route::get('', 'AdmController@index')->name("adm.home");
