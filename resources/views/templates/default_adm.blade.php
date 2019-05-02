52ee8b3bc54670426f72522176a3b1a2c9a6c150
MACL <mateusalisson15@gmail.com>
2019-05-02 01:16:02 -0300

    alterados

--- resources/views/templates/default_adm.blade.php
+++ resources/views/templates/default_adm.blade.php
@@ -24,18 +24,22 @@
         <div class="row altura">
             <div class="col-md-2  bg-info">
                 <div class="text-center">
-                    <h2 class="text-white py-2 border-bottom">Projeto Seja</h2>
+                    <h2 class="text-white py-2 border-bottom">ALMA</h2>
                 </div>     
-                <div class="list-group mt-3">
-                    <a class="btn btn-info text-white text-left" href="{{route('adm.collections.list')}}">Tipos de pagamentos</a>
-                    <a class="btn btn-info text-white text-left" href="{{route('adm.subscriptions.types.list')}}">Tipos de planos</a>
-                    <a class="btn btn-info text-white text-left" href="{{route('adm.subscriptions.plans.list')}}">Tipos de assinaturas</a>
-                    <a class="btn btn-info text-white text-left" href="{{route('adm.congresses.types.list')}}">Tipos de congressos</a>
-                    <a class="btn btn-info text-white text-left" href="{{route('adm.events.types.list')}}">Tipos de eventos</a> 
-                     <a class="btn btn-info text-white text-left" href="{{route('adm.security.profile.list')}}">Tipos de acessos</a>
-
-                    <a class="btn btn-info text-white text-left" href="{{route('site.logout')}}">Sair</a>
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Configurar tipos de:
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                        <a class="dropdown-item" href="{{route('adm.collections.list')}}">Tipos de pagamentos</a>
+                        <a class="dropdown-item" href="{{route('adm.subscriptions.types.list')}}">Tipos de planos</a>
+                        <a class="dropdown-item" href="{{route('adm.subscriptions.plans.list')}}">Tipos de assinaturas</a>
+                        <a class="dropdown-item" href="{{route('adm.congresses.types.list')}}">Tipos de congressos</a>
+                        <a class="dropdown-item" href="{{route('adm.events.types.list')}}">Tipos de eventos</a> 
+                        <a class="dropdown-item" href="{{route('adm.security.profile.list')}}">Perfis de acessos</a>
+                    </div>
                 </div>
+                <a class="btn btn-warning fixed-bottom col-md-2 text-white" href="{{route('site.logout')}}">Sair</a>
             </div>
             <div class="content col-md-10">
                 @yield('content')
