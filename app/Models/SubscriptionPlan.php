0565fc5a8b57027fe35d8701129d18732ba138bd
Mayke Alex <mayke.suporte@gmail.com>
2019-05-03 10:15:39 -0400

    Model  SubscriptionPlan ->  new e Update

--- app/Models/SubscriptionPlan.php
+++ app/Models/SubscriptionPlan.php
@@ -13,4 +13,40 @@ class SubscriptionPlan extends Model
     public function collection(){
         return $this->belongsTo(Collection::class);
     }
+
+    public function newSubscriptionPlan($dados):Array {
+        $this->collection_id = $dados['collection_id'];
+        $this->subscriptionType_id = $dados['subscriptionType_id'];
+        $save = $this->save();
+        
+        if ($save){
+            return [
+                'success' => true,
+                'message' => 'O cadastro realizado com sucesso!'
+            ];
+        }
+
+        return [
+            'success' => false,
+            'message' => 'Não foi possível realizar este cadastro. Verifique!'
+        ];
+    }
+
+    public function updateSubscriptionPlan($dados):Array {
+        $this->collection_id = $dados['collection_id'];
+        $this->subscriptionType_id = $dados['subscriptionType_id'];
+        $save = $this->save();
+
+        if ($save){
+            return [
+                'success' => true,
+                'message' => 'O cadastro realizado com sucesso!'
+            ];
+        }
+
+        return [
+            'success' => false,
+            'message' => 'Não foi possível realizar este cadastro. Verifique!'
+        ];
+    }
 }
