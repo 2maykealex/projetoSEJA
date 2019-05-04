c1dc2dfd6f368c2ff11cfb259fc35718cf14f43e
Mayke Alex <mayke.suporte@gmail.com>
2019-05-04 02:54:53 -0400

    Create alerts.blade.php

---
+++ resources/views/includes/alerts.blade.php
@@ -0,0 +1,38 @@
+@if ($errors->any())
+    <div class="alert alert-warning alert-dismissible"  role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        @foreach ($errors->all() as $error)
+            <p>{{ $error }}</p>
+        @endforeach
+    </div>
+@endif
+
+
+@if (session('success'))
+    <div class="alert alert-success alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        {{ session('message') }}
+    </div>
+@else
+    <div class="alert alert-danger alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        {{ session('message') }}
+    </div>
+
+@endif
+
+@if (session('error'))
+    <div class="alert alert-danger alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        {{ session('error') }}
+    </div>
+@endif
+
