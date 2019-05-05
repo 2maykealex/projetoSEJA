cc4e96178ac67d536cb166939b1b768251456a81
Mayke Alex <mayke.suporte@gmail.com>
2019-05-05 12:26:18 -0400

    Publisher inicial

--- app/Http/Controllers/Publisher/PublisherController.php
+++ app/Http/Controllers/Publisher/PublisherController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Publisher;
 
 use App\Models\Publisher;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class PublisherController extends Controller
@@ -14,7 +15,7 @@ class PublisherController extends Controller
      */
     public function index()
     {
-        //
+        return view('publisher.index');
     }
 
     /**
---
+++ resources/views/publisher/index.blade.php
@@ -0,0 +1,13 @@
+@extends('templates.default_publisher')
+
+@section('title')
+
+@endsection
+
+@section('content')
+
+	<div class="col-md-10 text-center">
+		<h1>BEM VINDO</h1>
+	</div>
+
+@endsection
