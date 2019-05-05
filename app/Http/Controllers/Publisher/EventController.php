f287b633e862523320a4350130377d555f7a9c36
Mayke Alex <mayke.suporte@gmail.com>
2019-05-05 12:45:31 -0400

    add use ....Controller

--- app/Http/Controllers/Publisher/EventController.php
+++ app/Http/Controllers/Publisher/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Publisher;
 
 use App\Models\Event;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
--- app/Http/Controllers/Publisher/PostController.php
+++ app/Http/Controllers/Publisher/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Publisher;
 
 use App\Models\Post;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
