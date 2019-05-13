28543147bac3447bf4c61365ed7d18a0e439c063
Mayke Alex <mayke.suporte@gmail.com>
2019-05-13 10:06:50 -0400

    Criado relacionamento targetAudience (Publico Alvo)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function targetAudience(){
+        return $this->belongsTo(SubscriptionPlan::class, 'subscription_plans_id');
+    }
 }
