fa731882de4d6ab3f182b46fa53fdeb94d475170
Mayke Alex <mayke.suporte@gmail.com>
2019-05-13 10:04:40 -0400

    Alterado o tipo do ID

--- database/migrations/2019_05_02_035026_create_congresses_table.php
+++ database/migrations/2019_05_02_035026_create_congresses_table.php
@@ -14,7 +14,7 @@ class CreateCongressesTable extends Migration
     public function up()
     {
         Schema::create('congresses', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->timestamps();
         });
     }
