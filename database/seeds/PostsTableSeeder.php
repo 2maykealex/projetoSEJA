c258d165e6566e55f4ad90e70fe2b2f69139604a
Mayke Alex <mayke.suporte@gmail.com>
2019-05-18 00:29:59 -0400

    Correção no seeder -> campo image

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -13,19 +13,22 @@ class PostsTableSeeder extends Seeder
     public function run()
     {
         Post::create([
-            'subscription_plans_id'=> 1,
+            'subscription_type_id'=> 1,
+            'publisher_id'=> 1,
             'title'     => 'Uma linda Mulher',
             'summary' => 'Durante uma viagem de negócios a Los Angeles, Edward...',
             'text' => 'Sempre haverá aqueles filmes que, sempre que você assistir, você vai sentir uma sensação estranhamente mágica...',
-            'image' => 'post_001',
+            'image' => 'post_0001.jpg',
         ]);
 
         Post::create([
-            'subscription_plans_id'=> 2,
+            'subscription_type_id'=> 2,
+            'publisher_id'=> 1,
             'title'     => 'Os Vingadores - O Ultimato',
             'summary' => 'Após Thanos eliminar metade das criaturas vivas...',
             'text' => 'Mais do que qualquer filme precedente do Universo Cinematográfico Marvel...',
-            'image' => 'post_002',
+            'image' => 'post_0002.jpg',
+
         ]);
     }
 }
