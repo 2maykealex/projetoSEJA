7adca1ca6eefd132aa819997838fde9bd23e6695
Mayke Alex <mayke.suporte@gmail.com>
2019-05-21 10:20:08 -0400

    Model Post -> newPost

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,4 +9,32 @@ class Post extends Model
     public function targetAudience(){
         return $this->belongsTo(SubscriptionPlan::class, 'subscription_plans_id');
     }
+
+    public function newPost($dados):Array {
+        $user = auth()->user();
+        $person = Person::where('user_id', $user->id)->get()->first();
+        $publisher = Publisher::where('person_id', $person->id)->first();
+
+        $this->title    = $dados['title'];
+        $this->summary  = $dados['summary'];
+        $this->text     = $dados['text'];
+        $this->image    = $dados['image'];
+        $this->subscription_type_id    = $dados['subscription_type_id'];
+        $this->publisher_id    = $publisher->id;
+        // $this->summary  = $dados['summary'];
+
+        $save = $this->save();
+
+        if ($save){
+            return [
+                'success' => true,
+                'message' => 'O cadastro realizado com sucesso!'
+            ];
+        }
+
+        return [
+            'success' => false,
+            'message' => 'Não foi possível realizar este cadastro. Verifique!'
+        ];
+    }
 }
