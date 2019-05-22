44d1d946145c78edfe158e574ed9397df6a9aafa
Mayke Alex <mayke.suporte@gmail.com>
2019-05-22 00:05:16 -0400

    UpdatePost

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\Person;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
@@ -11,9 +13,8 @@ class Post extends Model
     }
 
     public function newPost($dados):Array {
-        $user = auth()->user();
-        $person = Person::where('user_id', $user->id)->get()->first();
-        $publisher = Publisher::where('person_id', $person->id)->first();
+        $publisher = auth()->user()->person->publisher;
+        // dd($publisher);
 
         $this->title    = $dados['title'];
         $this->summary  = $dados['summary'];
@@ -35,6 +36,33 @@ class Post extends Model
         return [
             'success' => false,
             'message' => 'Não foi possível realizar este cadastro. Verifique!'
+        ];
+    }
+
+    public function updatePost($dados):Array {
+        $publisher = auth()->user()->person->publisher;
+        // $person = Person::where('user_id', $user->id)->get()->first();
+        // $publisher = Publisher::where('person_id', $person->id)->first();
+
+        $this->title    = $dados['title'];
+        $this->summary  = $dados['summary'];
+        $this->text     = $dados['text'];
+        $this->image    = $dados['image'];
+        $this->subscription_type_id    = $dados['subscription_type_id'];
+        $this->publisher_id    = $publisher->id;
+
+        $save = $this->save();
+
+        if ($save){
+            return [
+                'success' => true,
+                'message' => 'O cadastro atualizado com sucesso!'
+            ];
+        }
+
+        return [
+            'success' => false,
+            'message' => 'Não foi possível atualizar este cadastro. Verifique!'
         ];
     }
 }
