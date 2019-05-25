1edaccc18a408a671cd650b71bd55fe5dfd5663f
Mayke Alex <mayke.suporte@gmail.com>
2019-05-25 09:04:55 -0400

    Publisher-> model-> Event -> newEvent e updateEvent

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -6,5 +6,54 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    public function newEvent($dados):Array {
+        $publisher = auth()->user()->person->publisher;
+        $this->title    = $dados['title'];
+        $this->summary  = $dados['summary'];
+        $this->text     = $dados['text'];
+        $this->image    = $dados['image'];
+        $this->subscription_type_id    = $dados['subscription_type_id'];
+        $this->publisher_id    = $publisher->id;
+
+        $save = $this->save();
+
+        if ($save){
+            return [
+                'success' => true,
+                'message' => 'O Evento foi cadastrado com sucesso!'
+            ];
+        }
+
+        return [
+            'success' => false,
+            'message' => 'Não foi possível cadastrar este evento. Verifique!'
+        ];
+    }
+
+    public function updatePost($dados):Array {
+        $publisher = auth()->user()->person->publisher;
+        $this->title    = $dados['title'];
+        $this->summary  = $dados['summary'];
+        $this->text     = $dados['text'];
+        $this->subscription_type_id    = $dados['subscription_type_id'];
+        if (isset($dados['image'])){
+            $this->image    = $dados['image'];
+        }
+        $this->subscription_type_id    = $dados['subscription_type_id'];
+        $this->publisher_id    = $publisher->id;
+
+        $save = $this->save();
+
+        if ($save){
+            return [
+                'success' => true,
+                'message' => 'O evento foi atualizado com sucesso!'
+            ];
+        }
+
+        return [
+            'success' => false,
+            'message' => 'Não foi possível atualizar este evento. Verifique!'
+        ];
+    }
 }
