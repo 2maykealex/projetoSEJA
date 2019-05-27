ce2c0d40b4627906127359ad886ce42935a84f73
Mayke Alex <mayke.suporte@gmail.com>
2019-05-27 00:14:23 -0400

    Publisher-> Controller-> Posts -> estrutura

--- database/migrations/2019_05_27_035516_create_posts_table.php
+++ database/migrations/2019_05_27_035516_create_posts_table.php
@@ -22,6 +22,9 @@ class CreatePostsTable extends Migration
             $table->integer('publisher_id')->unsigned();
             $table->foreign('publisher_id')->references('id')->on('publishers');
 
+            $table->integer('report_type_id')->unsigned();
+            $table->foreign('report_type_id')->references('id')->on('report_types');
+
             $table->string('title', 80);
             $table->string('summary', 80);
             $table->string('text');
