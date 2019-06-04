5be3fc4a00f5d5a93d3b66d79d9eaed4b459c96f
Mayke Alex <mayke.suporte@gmail.com>
2019-06-03 23:18:15 -0400

    Create SiteFunction.php

---
+++ app/Http/Controllers/SiteFunction.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PostType;
+
+class SiteFunction extends Controller
+{
+    public function checkPostType($type){
+        $postType = PostType::where('name', $type)->get()->first();
+        if ($type == $postType->name) {
+            return $postType->id;
+        }
+    }
+}
