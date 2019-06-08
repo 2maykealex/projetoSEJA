ce38eb4ccb1681b9eebefd3396e04840cf73c7a5
Mayke Alex <mayke.suporte@gmail.com>
2019-06-08 10:28:19 -0400

    Publisher -> Event e Congress -> Somente list e show

--- app/Http/Controllers/Publisher/CongressController.php
+++ app/Http/Controllers/Publisher/CongressController.php
@@ -33,98 +33,10 @@ class CongressController extends Controller
         return view('publisher.congress.new', compact('subscriptionTypes','publisher'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $data = $request->all();
-        $data['image'] = $request->image->getClientOriginalName();
-
-        if($request->hasFile('image') && $request->file('image')->isValid() ){
-            $date = date('Y-m-d-H-i');
-            $name = kebab_case($date).'-'.$request->image->hashName();
-            $extension = $request->image->extension();
-            $nameFile  = "{$name}.{$extension}";
-
-            $data['image'] = $nameFile;
-            $upload = $request->image->storeAs('img/congresses', $nameFile);
-
-            if(!$upload)
-                return redirect()->back()->with('error', 'Falha ao enviar a imagem');
-        }
-        
-        $congress = new Congress;
-        $message = $congress->newCongress($data);
-        return redirect()->back()->with($message);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Congress  $congress
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $congress = Congress::where('id', $id)->get()->first();
         $subscriptionType = SubscriptionType::where('id', $congress->subscription_type_id)->orderby('id')->get()->first();
         return view('publisher.congress.show', compact('congress', 'subscriptionType'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Congress  $congress
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        $congress = Congress::where('id', $id)->get()->first();
-        $subscriptionTypes = SubscriptionType::orderby('id')->get();
-        return view('publisher.congress.edit', compact('congress', 'subscriptionTypes'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Congress  $congress
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Congress $congress)
-    {
-        $data = $request->all();
-
-        if($request->hasFile('image') && $request->file('image')->isValid() ){
-            $date = date('Y-m-d-H-i');
-            $name = kebab_case($date).'-'.$request->image->hashName();
-            $extension = $request->image->extension();
-            $nameFile  = "{$name}.{$extension}";
-
-            $data['image'] = $nameFile;
-            $upload = $request->image->storeAs('img/congresses', $nameFile);
-
-            if(!$upload)
-                return redirect()->back()->with('error', 'Falha ao enviar a imagem');
-        }
-        
-        $congress = Congress::where('id', $data['id'])->get()->first();
-        $message = $congress->updateCongress($data);
-        return redirect()->back()->with($message);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Congress  $congress
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Congress $congress)
-    {
-        //
     }
 }
