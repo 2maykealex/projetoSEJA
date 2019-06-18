2218b06a442b83e6eb3ca4927806d2476e195434
MACL <mateusalisson15@gmail.com>
2019-06-18 20:14:42 -0300

    retirados listagem de congressos e eventos

--- resources/views/templates/default_publisher.blade.php
+++ resources/views/templates/default_publisher.blade.php
@@ -184,7 +184,6 @@
                             </button>
                             <div class="collapse mt-1" id="menuEventos">
                                 <div class="nav flex-column border">
-                                    <a class="nav-link text-light" href="{{route('publisher.events.list')}}">Listagem</a>
                                 </div>
                             </div>
                         </li>
