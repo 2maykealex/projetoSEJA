6876e16282cfad3c0588c1fe61d22e726c123955
Mayke Alex <mayke.suporte@gmail.com>
2019-06-28 03:10:24 -0400

    subscription -> PostController -> index e show

---
+++ app/Http/Controllers/Subscriber/PostController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Subscriber;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\SubscriptionType;
+use App\Models\ReportType;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $person = auth()->user()->person;
+        $subscription_type_id = $person->subscriptionRegistry->subscriptionPlan->subscription_type_id;
+        $posts = Post::where('subscription_type_id', $subscription_type_id)->where('deleted', 0)->orderby('id', 'desc')->get();
+        return view('subscriber.post.index', compact('posts'));
+    }
+
+    public function show($id)
+    {
+        $person = auth()->user()->person;
+        $subscription_type_id = $person->subscriptionRegistry->subscriptionPlan->subscription_type_id;
+        $post = Post::where('subscription_type_id', $subscription_type_id)->where('id', $id)->get()->first();
+        if ($post){
+            return view('subscriber.post.show', compact('post'));
+        }
+        return redirect()->back()->with(['error','A página que tentou acessar não está acessível!']);
+    }
+}
