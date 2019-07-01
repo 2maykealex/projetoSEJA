2249f0853b9035b09d216b6fc400b8104fe761d5
Mayke Alex <mayke.suporte@gmail.com>
2019-07-01 09:09:48 -0400

    alterado id do select -> subscription_plan_id

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -123,7 +123,7 @@
                                     </div> 
                                 </div>
                                 <div class="form-group">
-                                    <select id="subscription_plans" name="subscription_plans" class="form-control">
+                                    <select id="subscription_plan_id" name="subscription_plan_id" class="form-control">
                                         <option value="-1">Planos</option>
                                         @foreach($subscriptionPlans as $subscriptionPlan)
                                             <option value="{{$subscriptionPlan->id}}">{{$subscriptionPlan->type->name}} - {{$subscriptionPlan->collection->name}} - R$ <?php echo number_format($subscriptionPlan->price,2, ',', '.');?></option>
