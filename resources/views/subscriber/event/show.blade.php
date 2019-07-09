cc2cd7067f394e860dab0faa962c0b743d0aabd5
Mayke Alex <mayke.suporte@gmail.com>
2019-07-09 09:25:54 -0400

    views\subscriber\congress\show  E views\subscriber\event\show - ajustes básicos

--- resources/views/subscriber/event/show.blade.php
+++ resources/views/subscriber/event/show.blade.php
@@ -1,31 +1,12 @@
 @extends('templates.default_subscriber')
 
 @section('title')
-
+    EVENTOS
 @endsection
 
 @section('content')
 	<div class="container">
-            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img class="d-block w-100" src="image/astro.jpg" alt="Primeiro Slide" height="400px">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="image/astro.jpg" alt="Segundo Slide" height="400px">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="image/astro.jpg" alt="Terceiro Slide" height="400px">
-                    </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Anterior</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Próximo</span>
-                </a>
+            <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark heigt-test">
             </div>
         </div>
 
@@ -36,44 +17,13 @@
 
                     <div class="blog-post">
                         <div class="text-center py-4">
-                            <h2 class="blog-post-title">SOBRE O CONGRESSO</h2>
+                            <h2 class="blog-post-title">EVENTO</h2>
                         </div>
                         <p class="blog-post-meta">Escrito por <a href="#">Fulano</a>, em 10 de Janeiro de 2010 - 10h10</p>
                         <small class="text-muted">Atualizado a 10 mins</small>
-                        <p class="mt-3">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
-                    </div><!-- /.blog-post -->
-
-                    <div class="blog-post">
-                        <div class="text-center py-4">
-                            <h2 class="blog-post-title">PALESTRANTES</h2>
-                        </div>
-
-                        <div class="card-group py-3 px-3 border">
-                            <a class="card col-md-3 border-white" href="#">
-                                <img class="card-img-top rounded-circle" src="image/bebe.jpg" alt="bebê raivoso" height="170px">
-                                <div class="card-body text-center">
-                                    <p class="card-text">Lorem Ipsum</p>
-                                </div>
-                            </a>
-                            <a class="card col-md-3 border-white" href="#">
-                                <img class="card-img-top rounded-circle" src="image/bebe.jpg" alt="bebê raivoso" height="170px">
-                                <div class="card-body text-center">
-                                    <p class="card-text">Lorem Ipsum</p>
-                                </div>
-                            </a>
-                            <a class="card col-md-3 border-white" href="#">
-                                <img class="card-img-top rounded-circle" src="image/bebe.jpg" alt="bebê raivoso" height="170px">
-                                <div class="card-body text-center">
-                                    <p class="card-text">Lorem Ipsum</p>
-                                </div>
-                            </a>
-                            <a class="card col-md-3 border-white" href="#">
-                                <img class="card-img-top rounded-circle" src="image/bebe.jpg" alt="bebê raivoso" height="170px">
-                                <div class="card-body text-center">
-                                    <p class="card-text">Lorem Ipsum</p>
-                                </div>
-                            </a>
-                        </div>
+                        <p class="mt-3">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro</p>
+                        <img src="image/astro.jpg" class="img-fluid py-3" alt="Responsive image" width="100%"> 
+                        <p>quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
                     </div><!-- /.blog-post -->
 
             
@@ -99,4 +49,6 @@
                 </div><!-- /.row -->
             </div>
         </main>
+    </div>
+
 @endsection
