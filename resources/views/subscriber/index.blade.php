fe5f8541baaa5f63d8dbd804df63524381b46f53
Mayke Alex <mayke.suporte@gmail.com>
2019-07-09 23:06:58 -0400

    views\subscriber\index -> Listar todos os eventos, congressos e cursos

--- resources/views/subscriber/index.blade.php
+++ resources/views/subscriber/index.blade.php
@@ -1,13 +1,13 @@
 @extends('templates.default_subscriber')
 
 @section('title')
-
+	Home
 @endsection
 
 @section('content')
 
 	<div class="col-md-10 text-center">
-		<h1>BEM VINDO {{explode(" ", auth()->user()->name)[0] }}</h1>
+		<h1>BEM VINDO(A) {{explode(" ", auth()->user()->name)[0] }}</h1>
 	</div>
 
 	@if (!$posts->isEmpty())
@@ -21,6 +21,26 @@
 							<div class="card-img-overlay">
 								<h5 class="card-title">{{$post->author->name}}</h5>
 								<p class="position-people">{{$post->summary}}</p>
+							</div>
+						</div>
+					</div>
+				@endforeach
+			</div>
+		</div>
+	@endif
+
+	@if (!$events->isEmpty())
+		{{"EVENTOS - CONGRESSOS - CURSOS"}}
+		<div class="container-fluid">
+			<div class="carousel carousel-us">
+				@foreach ($events as $event)
+					<div class="col-md-12">
+						<div class="card text-white imgzoom ">
+							<img class="card-img mb-3 " src="{{ asset('storage/img/profiles/' . $event->author->image)}}" alt="Card image" height="350">
+							<div class="card-img-overlay">
+								<h5>{{$event->type->name}}</h5>
+								<h5 class="card-title">{{$event->author->name}}</h5>
+								<p class="position-people">{{$event->summary}}</p>
 							</div>
 						</div>
 					</div>
