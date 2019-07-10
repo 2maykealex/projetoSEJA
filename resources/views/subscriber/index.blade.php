5814e8574b0c6d7452ce71c939e26c486b0e75dc
MACL <mateusalisson15@gmail.com>
2019-07-10 00:45:11 -0300

    Carousel de posts add e perfil top-right com rotas

--- resources/views/subscriber/index.blade.php
+++ resources/views/subscriber/index.blade.php
@@ -6,14 +6,14 @@
 
 @section('content')
 
-	<div class="col-md-10 text-center">
+	<!--<div class="col-md-10 text-center">
 		<h1>BEM VINDO {{explode(" ", auth()->user()->name)[0] }}</h1>
 	</div>
 
 	@if (!$posts->isEmpty())
 		{{"POSTS"}}
 		<div class="container-fluid">
-			<div class="carousel carousel-us">
+			<div class="carousel">
 				@foreach ($posts as $post)
 					<div class="col-md-12">
 						<div class="card text-white imgzoom ">
@@ -27,5 +27,140 @@
 				@endforeach
 			</div>
 		</div>
-	@endif
+	@endif-->
+
+	@if (!$posts->isEmpty())
+        <div class="container-fluid" id="publicacao">
+            <div class="pricing-header py-4 pt-md-5 pb-md-4 mx-auto text-center">
+                <h1 class="display-5 mt-5 mb-3">Últimas postagens</h1>
+            </div>
+
+        
+            <div class="carousel carousel-content">
+                @foreach($posts as $post)
+                <div class="alto col-lg-12 mb-2">
+                    <div class="card my-3 shadow rounded">
+                        <div class="hoverzoom">
+                            <img src="{{ asset('storage/img/posts/' . $post->image)}}" alt="..." class="card-img-top">
+                            <div class="retina text-right">
+                                <button type="submit" class="btn btn-light">icone</button>
+                            </div>
+                        </div>
+                        <div class="row no-gutters px-3 pb-3">
+                            <div class="card-body">
+                                <a class="card-title h6" href="#">{{$post->title}}</a>
+                            </div>    
+                            <div class="position-profile">
+                                <a href="#"><img class="rounded-circle abs" src="{{ asset('storage/img/profiles/' . $post->author->image)}}" width="40" height="40"></a>
+                            </div>  
+                            <div class="position-people">
+                                <small class="text-muted">Por <a href="#">{{$post->author->name}}</a></small>
+                            </div>
+                            <div class="position-date-time">
+                                <small class="text-muted">Em {{date('d/m/Y', strtotime($post->created_at))}}</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+                <div class="alto col-lg-12 mb-2">
+                    <div class="card my-3 shadow rounded">
+                        <div class="hoverzoom">
+                            <img src="" alt="..." class="card-img-top">
+                            <div class="retina text-right">
+                                <button type="submit" class="btn btn-light">icone</button>
+                            </div>
+                        </div>
+                        <div class="row no-gutters px-3 pb-3">
+                            <div class="card-body">
+                                <a class="card-title h6" href="#"></a>
+                            </div>    
+                            <div class="position-profile">
+                                <a href="#"><img class="rounded-circle abs" src="" width="40" height="40"></a>
+                            </div>  
+                            <div class="position-people">
+                                <small class="text-muted">Por <a href="#"></a></small>
+                            </div>
+                            <div class="position-date-time">
+                                <small class="text-muted"></small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="alto col-lg-12 mb-2">
+                    <div class="card my-3 shadow rounded">
+                        <div class="hoverzoom">
+                            <img src="" alt="..." class="card-img-top">
+                            <div class="retina text-right">
+                                <button type="submit" class="btn btn-light">icone</button>
+                            </div>
+                        </div>
+                        <div class="row no-gutters px-3 pb-3">
+                            <div class="card-body">
+                                <a class="card-title h6" href="#"></a>
+                            </div>    
+                            <div class="position-profile">
+                                <a href="#"><img class="rounded-circle abs" src="" width="40" height="40"></a>
+                            </div>  
+                            <div class="position-people">
+                                <small class="text-muted">Por <a href="#"></a></small>
+                            </div>
+                            <div class="position-date-time">
+                                <small class="text-muted"></small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="alto col-lg-12 mb-2">
+                    <div class="card my-3 shadow rounded">
+                        <div class="hoverzoom">
+                            <img src="" alt="..." class="card-img-top">
+                            <div class="retina text-right">
+                                <button type="submit" class="btn btn-light">icone</button>
+                            </div>
+                        </div>
+                        <div class="row no-gutters px-3 pb-3">
+                            <div class="card-body">
+                                <a class="card-title h6" href="#"></a>
+                            </div>    
+                            <div class="position-profile">
+                                <a href="#"><img class="rounded-circle abs" src="" width="40" height="40"></a>
+                            </div>  
+                            <div class="position-people">
+                                <small class="text-muted">Por <a href="#"></a></small>
+                            </div>
+                            <div class="position-date-time">
+                                <small class="text-muted"></small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="alto col-lg-12 mb-2">
+                    <div class="card my-3 shadow rounded">
+                        <div class="hoverzoom">
+                            <img src="" alt="..." class="card-img-top">
+                            <div class="retina text-right">
+                                <button type="submit" class="btn btn-light">icone</button>
+                            </div>
+                        </div>
+                        <div class="row no-gutters px-3 pb-3">
+                            <div class="card-body">
+                                <a class="card-title h6" href="#"></a>
+                            </div>    
+                            <div class="position-profile">
+                                <a href="#"><img class="rounded-circle abs" src="" width="40" height="40"></a>
+                            </div>  
+                            <div class="position-people">
+                                <small class="text-muted">Por <a href="#"></a></small>
+                            </div>
+                            <div class="position-date-time">
+                                <small class="text-muted"></small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
 @endsection
--- resources/views/templates/default_subscriber.blade.php
+++ resources/views/templates/default_subscriber.blade.php
@@ -8,11 +8,17 @@
 
     <link rel="stylesheet" type="text/css" href="{{ asset('css/subscriber_style.css') }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!--<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>-->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick-theme.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/world_style.css') }}">
 
     <style>
         
     </style>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.js"></script>
 </head>
 <body>
 
@@ -20,7 +26,7 @@
         $url = explode("/", URL::current());
     ?>
     
-    <nav class="navbar navbar-expand shadow">
+    <nav class="navbar navbar-expand bg-white shadow fixed-top">
         <!--label com um botão que liga com o input-->  
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
@@ -30,15 +36,13 @@
         
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-                <a href="#"><img class="rounded-circle abs" src="3" width="40" height="40"></a>
+                <a href="#"><img class="rounded-circle abs" src="{{ asset('storage/img/profiles/' . auth()->user()->person->image)}}" width="40" height="40"></a>
             </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Nome
-                </a>
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{auth()->user()->person->name}}</a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">item 1</a>
-                    <a class="dropdown-item" href="#">item 2</a>
+                    <a class="dropdown-item" href="{{route('subscriber.profile')}}">Ver perfil</a>
+                    <a class="dropdown-item" href="{{route('subscriber.profile.edit')}}">Editar perfil</a>
                     <a class="dropdown-item" href="{{route('site.logout')}}">Sair</a>
                 </div>
             </li>
@@ -51,7 +55,7 @@
             <div class="flex-column">
                 <!--Input com checkbox para verificar sidebar--> 
                 <input type="checkbox" id="bt_menu">
-                <div class="sidebar shadow">
+                <div class="sidebar bg-white shadow">
                     <div class="sidebar-sticky">
                         <form class="form-inline mt-3 mb-4 ">
                             <input class="form-nav mr-sm-2 ml-3" type="search" placeholder="Pesquisar" aria-label="Search">
@@ -102,8 +106,8 @@
                 </div>
             </div>
             
-            <div class="col" style="padding: 0">        
-                <main role="main" class="p-5">
+            <div class="col" style="padding-left: 75px;">        
+                <main role="main" >
                     @include('includes.alerts')
                     @yield('content')
                 </main>
@@ -116,11 +120,51 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
     <script>
+        //script para colocar active na barra transversal da pagina do assinante
         $('.sidebar-sticky').on('click', 'li', function(){
             $('.sidebar-sticky ul li.active').removeClass('active');
             $(this).addClass('active');
         });
 
+
+        //script para colocar um carousel de posts
+         $('.carousel').slick({
+            infinite: true,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            centerMode: true,
+
+            responsive: [
+                {
+                  breakpoint: 1024,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                  }
+                },
+                {
+                  breakpoint: 600,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                  }
+                },
+                {
+                  breakpoint: 480,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                  }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
+
+        });
         
     </script>
 </body>
