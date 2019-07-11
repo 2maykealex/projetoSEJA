3331c86cdf6b5410037506293234f1ae0fb5ada9
Mayke Alex <mayke.suporte@gmail.com>
2019-07-11 00:57:56 -0400

    Controllers\Subscriber\SubscriberController -> corrigido  - salvar assinante

--- app/Http/Controllers/Subscriber/SubscriberController.php
+++ app/Http/Controllers/Subscriber/SubscriberController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers\Subscriber;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\User;
+use App\Models\Person;
+use App\Models\UserProfile;
+use App\Models\Subscriber;
+use App\Models\SubscriptionRegistry;
 
 class SubscriberController extends Controller
 {
@@ -25,6 +30,57 @@ class SubscriberController extends Controller
     public function edit($id)
     {
         //
+    }
+
+    public function store(Request $request){
+        $data = $request->all();
+        $userData = [];
+        $userData['name']     = $data['name'];
+        $userData['email']    = $data['email'];
+        $userData['password'] = $data['password'];
+        $userBd = User::where('email', $data['email'])->get()->first(); //verifica se existe o email de novo usuário no banco 
+        if (!$userBd){
+            $user = new User;
+            $idNewUser = $user->newUser($userData);
+            $idNewUser = $idNewUser[0];
+            if($idNewUser){
+                $personData = [];
+                $personData['name']      = $data['name'];
+                $personData['user_id']   = $idNewUser;
+                $personData['cpf']       = $data['cpf'];
+                $personData['telephone'] = $data['telephone'];
+                $personData['zipcode']   = $data['zipcode'];
+                $personData['address']   = $data['address'];
+                $personData['city_id']   = $data['city'];
+                $personData['image']     = "";
+                $person = new Person;
+                $idNewPerson = $person->newPerson($personData);
+                if($idNewPerson){
+                    $dados['user_id']          = $idNewUser;  #id do novo usuário
+                    $dados['user_profile_id']  = 2;        #subscriber
+                    $userProfile = new UserProfile;
+                    $saveUserProfile = $userProfile->newUserProfile($dados);
+                    if($saveUserProfile){
+                        $subscriberData['person_id'] = $idNewPerson[0];
+                        // $subscriberData['paymentMethod'] = "teste";
+                        $subscriber = new Subscriber;
+                        $saveSubscriber = $subscriber->newSubscriber($subscriberData);
+                        
+                        if($saveSubscriber){
+                            $subscriptionRegistryData['person_id'] = $idNewPerson[0];
+                            $subscriptionRegistryData['subscription_plan_id'] = $data['subscription_plan_id'];
+                            $subscriptionRegistryData['status'] = 0; 
+                            $subscriptionRegistryData['expires_in'] = "30/07/2019";
+                            $subscriptionRegistry = new SubscriptionRegistry;
+                            $saveSubscriptionRegistry = $subscriptionRegistry->newSubscriptionRegistry($subscriptionRegistryData);
+                            if($saveSubscriptionRegistry){
+                                return redirect()->route('site.home', ['success' => true, 'message' => 'Usuário salvo com sucesso!']);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     public function update(Request $request, $id)
