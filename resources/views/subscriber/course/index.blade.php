86a8d1ee60219836318c231b9066c7420da1cba5
MACL <mateusalisson15@gmail.com>
2019-07-17 02:05:36 -0300

    adicioados listagem e visualização de cursos, porem não finalizado

--- resources/views/subscriber/course/index.blade.php
+++ resources/views/subscriber/course/index.blade.php
@@ -5,35 +5,30 @@
 @endsection
 
 @section('content')
-	<div class="d-flex justify-content-center flex-lg-center py-5">
-		<div class="col-md-10 bg-light py-3">
-			<nav class="navbar navbar-expand-lg ">
-			  	<h2>CURSOS</h2>
-			</nav>
-	      	<div class="table-responsive pb-3 pt-3 bg-white px-3">
-	        	<table class="table table-bordered">
-	          		<thead>
-	            		<tr>
-	              			<th>DATA DE CRIAÇÃO</th>
-	              			<th>NOME</th>
-	              			<th>RESUMO</th>
-	              			<th class="text-center">AÇÕES</th>
-	            		</tr>
-	          		</thead>
-	          		<tbody>
-	          		@foreach($courses as $course)
-		                <tr>
-			                <td>{{date('d/m/Y', strtotime($course->created_at))}}</td>
-			                <td>{{$course->name}}</td>
-			                <td>{{$course->summary}}</td>
-			                <td class="actions text-center">
-		                		<a class="btn btn-success btn-sm button-width" href="{{route('subscriber.courses.show', $course->id)}}">Visualizar</a>
-			                 </td>
-		                </tr>
-		            @endforeach   
-	          		</tbody>
-	        	</table>
-	      	</div>
-	    </div>
-	</div>
+
+	<div class="container">
+        <div class="pricing-header text-center mt-3 mb-5 mx-auto">
+            <h1 class="display-5">Cursos</h1>
+        </div>
+        @if (!$courses->isEmpty())
+            @foreach($courses as $course)
+                <div class="border-bottom">
+                    <div class="row no-gutters my-4">
+                        <div class="col-md-3">
+                            <a href="curso.html"><img src="../image/lobos.jpg" class="card-img shadow my-1" alt="..."></a>
+                        </div>
+                        <div class="col-md-7 pl-3">
+                            <div class="card-body">
+                                <div class="card-title">
+                                    <a class="h5" href="curso.html">Lobos cinzentos são lindos e ferozes</a>
+                                </div>
+                                <p class="card-text text-left">Eu tenho medo de lobos porque são animais selvagens, porem são lindos</p>   
+                                <small class="text-muted mb-2">Por <a href="#">Mateus</a> em 10/10/2010</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        @endif
+    </div>
 @endsection
--- resources/views/subscriber/course/show.blade.php
+++ resources/views/subscriber/course/show.blade.php
@@ -5,67 +5,113 @@
 @endsection
 
 @section('content')
-	<div class="container">
-            <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark heigt-test">
-            </div>
-        </div>
-
-        <main role="main" class="container">
-          <div class="row">
-            <div class="d-flex justify-content-center flex-lg-center">
-                <div class="col-md-9 blog-main">
-
-                    <div class="blog-post">
-                        <div class="text-center py-4">
-                            <h2 class="blog-post-title">CURSO</h2>
-                        </div>
-                        <div class="table-responsive pb-3 pt-3 bg-white px-3">
-                            <table class="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>AULA</th>
-                                        <th class="text-center">AÇÕES</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($course->courseItens as $class)
-                                    <tr>
-                                        <td>{{$class->summary}}</td>
-                                        <td embed-responsive embed-responsive-16by9>
-                                            <iframe class="embed-responsive-item" width="420" height="245" 
-                                                src="{{$class->file}}">
-                                            </iframe>
-                                        </td>
-                                    </tr>
-                                    @endforeach   
-                                </tbody>
-                            </table>
-                            </div>
-                    </div><!-- /.blog-post -->
+    <!--@foreach($course->courseItens as $class)
+    <tr>
+        <td>{{$class->summary}}</td>
+        <td embed-responsive embed-responsive-16by9>
+            <iframe class="embed-responsive-item" width="420" height="245" 
+                src="{{$class->file}}">
+            </iframe>
+        </td>
+    </tr>
+    @endforeach --> 
+                                
 
             
-                    <aside class="blog-sidebar">
-                        <div class="p-3 mb-3 rounded">
-                            <h4 class="font-italic">Relacionados...</h4>
-                            <div class="col-md-6">
-                                <div class="card mb-4 shadow-sm mt-5">
-                                    <img class="card-img-top" src="image/astro.jpg" alt="Card image cap">
-                                    <div class="card-body">
-                                        <p class="card-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</p>
-                                    <div class="d-flex justify-content-between align-items-center">
-                                        <div class="btn-group">
-                                            <a class="btn btn-sm btn-outline-secondary">Ver tudo</a>
-                                        </div>
-                                        <small class="text-muted">Atualizado a 9 mins</small>
-                                    </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </aside><!-- /.blog-sidebar -->
-                </div><!-- /.row -->
+                    
+
+    <!-- Imagem de topo -->
+    <div class="bg-dark" style="height: 20rem;">
+        <div class="col">
+            <!--<img src="../image/lobos.jpg" class="card-img shadow my-1" alt="...">-->
+        </div>
+    </div>
+
+    <!-- Conteúdo -->
+    <div class="container mt-5">
+
+        <!-- Barra na vertical para troca de conteúdo -->
+        <ul class="nav nav-pills mb-5" id="myTab" role="tablist">
+           <!--<@foreach($subscriptionTypes as $subscriptionType)
+            <li class="nav-item">
+                <a class="nav-link mr-1 <?php if ($subscriptionType->id == 1) {echo 'active';}?>" id="{{$subscriptionType->name}}-tab" data-toggle="tab" href="#{{$subscriptionType->name}}" role="tab" aria-controls="{{$subscriptionType->name}}" aria-selected="true">{{$subscriptionType->name}}</a>
+            </li>
+            @endforeach-->
+            <li class="nav-item">
+                <a class="nav-link mr-1 active" id="descricao-tab" data-toggle="tab" href="#descricao" role="tab" aria-controls="descricao" aria-selected="true">Descricao</a>
+            </li>
+             <li class="nav-item">
+                <a class="nav-link mr-1 " id="conteudo-tab" data-toggle="tab" href="#conteudo" role="tab" aria-controls="conteudo" aria-selected="false">Contéudo</a>
+            </li>
+        </ul>
+
+        <!-- Conteúdo que será trocado de acordo com a barra vertical -->
+        <div class="tab-content " id="myTabContent">
+            
+            <!--@foreach($subscriptionTypes as $subscriptionType)
+                <div class="tab-pane fade show <?php if ($subscriptionType->id == 1) {echo 'active';}?>" id="{{$subscriptionType->name}}" role="tabpanel" aria-labelledby="{{$subscriptionType->name}}-tab">
+                    <div class="d-flex flex-wrap justify-content-around text-center mb-3">
+                        @foreach($subscriptionPlans as $subscriptionPlan)
+                            @if($subscriptionType->id == $subscriptionPlan->subscription_type_id)
+                                
+                            Contéudo do site
+
+                            @else
+
+                            @endif
+                          
+                        @endforeach
+                    </div>
+                </div>
+            @endforeach-->
+            <div class="tab-pane fade show active" id="descricao" role="tabpanel" aria-labelledby="descricao-tab">
+                <div class="d-flex flex-wrap mb-3">
+                    <!-- Falta colocar class para tratar textos em descrição -->
+                    <div>
+                        <h5>Descrição sobre o curso</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporary incididunt ut labore et dolore magna aliqua. O uso de um mínimo de exercícios, os exercícios nostrud ullamco</p>
+                    </div>
+                </div>
             </div>
-        </main>
+
+            <div class="tab-pane fade show" id="conteudo" role="tabpanel" aria-labelledby="conteudo-tab">
+                <div class="d-flex flex-wrap mb-3">
+                    <div class="col-12 bg-light p-0">
+                        <ul class="nav flex-column nav-ul">
+                            <li class="nav-li">
+                                <a class="nav-a" data-toggle="modal" data-target=".bd-example-modal-lg" style="cursor: pointer;">modal sobre o video</a>
+                            </li>
+                            <li class="nav-li">
+                                <a class="nav-a" style="cursor: pointer;">botão para expandir PDFs sobre o video</a>
+                                <ul class="nav nav-sub-ul">
+                                    <li class="nav-sub-li">
+                                        <a class="nav-sub-a">pdf 1</a>
+                                    </li>
+                                    <li class="nav-sub-li">
+                                        <a class="nav-sub-a">pdf 2</a>
+                                    </li>
+                                    <li class="nav-sub-li">
+                                        <a class="nav-sub-a">pdf 3</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <video width="100%" controls>
+                    <source src="../video/baleiazul.mp4" type="video/mp4" >
+                </video>
+            </div>
+        </div>
     </div>
 
 @endsection
