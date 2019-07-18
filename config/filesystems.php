a3a8af82c3bde70dc2d5c96c58cc56aea72ef938
Mayke Alex <mayke.suporte@gmail.com>
2019-07-18 08:57:01 -0400

    reset configs

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 'my_upload'),
+    'default' => env('FILESYSTEM_DRIVER', 'public'),
 
     /*
     |--------------------------------------------------------------------------
