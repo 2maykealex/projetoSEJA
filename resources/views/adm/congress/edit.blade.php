e3c755467cb8ccdc546957c0fae0e85d5e84c749
Mayke Alex <mayke.suporte@gmail.com>
2019-07-22 09:30:56 -0400

    update views\adm\congress\edit

--- resources/views/adm/congress/edit.blade.php
+++ resources/views/adm/congress/edit.blade.php
@@ -14,6 +14,7 @@
 				@csrf
 				<div class="form-group"> 
 				    <input type="hidden" class="form-control" id="id" name="id" value="{{$congress->id}}">
+				    <input type="hidden" class="form-control" id="event_type_id" name="event_type_id" value="{{$congress->type->id}}">
 				</div>
 				<div class="form-group">
 				    <label for="title">TÍTULO</label>   
@@ -28,19 +29,14 @@
 				    <textarea class="form-control" id="text" name="text" rows="8">{{$congress->text}}</textarea>
 				</div>
 				<div class="form-group">
-		    		<label for="subscription_type_id">TIPO DE PÚBLICO</label>	    		
-		      		<select class="form-control" id="subscription_type_id" name="subscription_type_id" collapse>
-		        		<option>Escolher</option>
-		        		@foreach ($subscriptionTypes as $subscriptionType)
-							<option value="{{$subscriptionType->id}}" 
-		        			@if ( isset($congress) && $congress->subscription_type_id == $subscriptionType->id ) selected @endif>{{$subscriptionType->name}}</option>
-		        		@endforeach	        		
-		      		</select>	
-		    	</div>
+					<label for="text">SERÁ REALIZADO EM:</label>
+					<input type="date" class="form-control" id="event_date" name="event_date" min="{{date('Y-m-d')}}" value="{{$congress->event_date}}">
+				</div>
 		    	<div class="form-group">
 				    <label for="image">IMAGEM</label>
 				    <div class="col-md-8 py-3"> 
-				    	<img src="{{ asset('storage/img/congresses/' . $congress->image) }}" width="100%" class="img-fluid "> 
+						
+				    	<img src="{{ asset('storage/img/events/' . $congress->image) }}" width="100%" class="img-fluid "> 
 				    </div> 
 				    <input type="file" class="form-control-file" id="image" name="image" accept="image/*" required>
 				</div>
@@ -52,12 +48,12 @@
 				    <label>ESCRITO POR</label>
 		            <div class="row no-gutters bg-light py-3 px-3">
 		                <div class="col-lg-1 text-center">
-		                    <img class="rounded-circle" src="{{ asset('storage/img/profiles/' . $person->image)}}" width="50" height="50">
+		                    <img class="rounded-circle" src="{{ asset('storage/img/profiles/' . $congress->author->image)}}" width="50" height="50">
 		                </div>
 		                <table class="col-lg-2 text-center" style=" height: 50px;">
 		                	<tbody>
 		                		<tr>
-		                			<td class="align-middle h5">{{$person->name}}</td>
+		                			<td class="align-middle h5">{{$congress->author->name}}</td>
 		                		</tr>
 		                	</tbody>
 		                </table>	                
