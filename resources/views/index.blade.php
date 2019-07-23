cfd91e5dff98579b6c1a16e9873517e05b84630c
MACL <mateusalisson15@gmail.com>
2019-07-23 00:32:23 -0300

    diminuindo imagens do modal e retirando texto do mesmo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -216,11 +216,11 @@
                             <div class="modal-body">
                                 <div class="container-fluid" style="padding-right: 0; padding-left: 0;">
                                     <div class="d-flex justify-content-center">
-                                        <img src="{{asset('storage/img/posts/'. $post->image)}}" class="img-fluid col-sm-12 col-md-7 px-0" alt="Responsive image">  
+                                        <img src="{{asset('storage/img/posts/'. $post->image)}}" class="img-fluid col-sm-12 col-md-6 px-0" alt="Responsive image">  
                                     </div>
                                     <div class="container">
                                         <div class="row d-flex justify-content-center">
-                                            <div class="col-10 col-md-7 blog-main">
+                                            <div class="col-10 col-md-6 blog-main">
                                                 <div class="blog-post">
                                                     <div class="text-center pt-5">
                                                         <div class="blog-post-title h2">{{$post->title}}</div>
@@ -229,7 +229,6 @@
                                                         <div class="blog-post-title h4">"{{$post->summary}}"</div>
                                                     </div>
                                                     <p class="blog-post-meta">Publicado por <a href="#">{{$post->author->name}}</a>, em {{date('d/m/Y', strtotime($post->created_at))}}</p>
-                                                    <p class="mt-3">{{$post->text}}</p>
                                                 </div>
                                             </div>
                                         </div>
